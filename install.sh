5c1c6ec0c14f6a4ed87484f6cef2a7e6803b4ced
Dev <dnb.ranjikaa@gmail.com>
2021-04-19 11:30:58 +0530

    finished install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #setting up selenium using pip3 and installing google chrome 
-
+sudo apt install unzip
 pip3 install selenium
 sudo apt update
 CURR=$(pwd)
@@ -8,3 +8,9 @@ cd /home/$USER/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 cd $CURR
+google-chrome --version
+echo 'Please find and install the suitable version webdriver for selenium'
+echo 'https://chromedriver.chromium.org/getting-started'
+read -p 'Press enter to continue'
+echo 'done'
+python3 req.py
